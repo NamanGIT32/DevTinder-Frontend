33f14ce3c6ff58d968f40a3707542c92f832058b
Naman Paliwal <112724171+NamanGIT32@users.noreply.github.com>
2025-03-13 11:38:05 +0530

    Initialized frontend with vite and installed tailwind and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+const App = () => {
+
+  return (
+    <>
+      <Navbar/>
+      <div className="text-4xl font-bold underline">
+      Hello world!
+    </div>
+    </>
+  ) 
+}
+
+export default App
